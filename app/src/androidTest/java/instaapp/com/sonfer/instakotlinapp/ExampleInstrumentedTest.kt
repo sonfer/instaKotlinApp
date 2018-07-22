9f4c18763c737b1028fb2714441edeafac1ccd24
Sonfer DER <sonferder@gmail.com>
2018-07-22 14:32:04 +0300

    initial

---
+++ app/src/androidTest/java/instaapp/com/sonfer/instakotlinapp/ExampleInstrumentedTest.kt
@@ -0,0 +1,24 @@
+package instaapp.com.sonfer.instakotlinapp
+
+import android.support.test.InstrumentationRegistry
+import android.support.test.runner.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getTargetContext()
+        assertEquals("instaapp.com.sonfer.instakotlinapp", appContext.packageName)
+    }
+}
